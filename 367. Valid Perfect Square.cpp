8129e4698b311ef961b0c29ef78f8cf50abfd2bb
Crushro <1309769300@qq.com>
2021-11-04 14:03:29 +0800

    2021/11/4 Study

---
+++ 367. Valid Perfect Square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(int i = 1; i <= 46340; i++) {
+            if(i * i > num) {
+                return false;
+            } else if(i * i == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
