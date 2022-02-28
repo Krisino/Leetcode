066d60d87b4e287cc646defeb87a98c27d31a662
Krisino <1309769300@qq.com>
2022-02-28 15:29:35 +0800

    2022/02/28 Study

---
+++ 733. Flood Fill.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& image, int sr, int sc, int oldColor, int newColor) {
+        if(sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size()) {
+            return;
+        }
+        if(image[sr][sc] != oldColor) {
+            return;
+        }
+        image[sr][sc] = newColor;
+        dfs(image, sr - 1, sc, oldColor, newColor);
+        dfs(image, sr + 1, sc, oldColor, newColor);
+        dfs(image, sr, sc - 1, oldColor, newColor);
+        dfs(image, sr, sc + 1, oldColor, newColor);
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        if(oldColor == newColor) {
+            return image;
+        }
+        dfs(image, sr, sc, oldColor, newColor);
+        return image;
+    }
+};
